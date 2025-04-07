fb0435f9494429108dff2e87e23fb8f0d0e53955
Sandra Arato <sandra@idoescode.com>
2025-04-08 08:30:43 +1000

    trying to setup db again

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,13 +1,14 @@
-// import postgres from 'postgres';
+import postgres from 'postgres';
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 // async function listInvoices() {
 // 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
+//     -- SELECT invoices.amount, customers.name
+//     -- FROM invoices
+//     -- JOIN customers ON invoices.customer_id = customers.id
+//     -- WHERE invoices.amount = 666;
+//     DROP TABLE invoices
 //   `;
 
 // 	return data;
