59616bc71e1380c3585dd2b64448fcf4cf736e28
Sandra Arato <sandra@idoescode.com>
2025-04-16 08:45:04 +1000

    fixed typo

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -33,6 +33,6 @@ export function DeleteInvoice({ id }: { id: string }) {
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
       </button>
-    </>
+    </form>
   );
 }
